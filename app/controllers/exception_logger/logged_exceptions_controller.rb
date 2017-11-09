068e422f125bd38436310fa7e05238bd6ba0f15e
Eduardo Azevedo <eduardoazevedo3@gmail.com>
2017-11-09 18:10:50 -0200

    Fix delete visible errors

--- app/controllers/exception_logger/logged_exceptions_controller.rb
+++ app/controllers/exception_logger/logged_exceptions_controller.rb
@@ -68,7 +68,7 @@ module ExceptionLogger
     end
 
     def destroy_all
-      LoggedException.delete_all(:id => params[:ids]) unless params[:ids].blank?
+      LoggedException.where(:id => params[:id]).delete_all unless params[:ids].blank?
       query
     end
 
